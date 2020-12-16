724102b5ca193ac95f2f27c77988f719d5c939ec
Karen Acosta <kar3n.acosta@gmail.com>
2020-12-15 19:57:45 -0600

    adding content to lib folder

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require('./Employee');
+
+class Engineer extends Employee {
+    constructor(name, id, email, gitHub) {
+        super(name, id, email);
+        this.gitHub = gitHub;
+    }
+    getName() {
+        return this.name;
+    }
+    getID() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getGitHub() {
+        return this.gitHub;
+    }
+    getRole() {
+        return 'Engineer';
+    }
+}
+
+module.exports = Engineer;
